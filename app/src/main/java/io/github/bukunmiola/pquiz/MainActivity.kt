887152c08f04e7aaa2708fc5ec6e0213f68f9d14
BukunmiOla <ebukunmiola@gmail.com>
2020-10-29 22:33:10 +0100

    feat: navition funtionality fixed

--- app/src/main/java/io/github/bukunmiola/pquiz/MainActivity.kt
+++ app/src/main/java/io/github/bukunmiola/pquiz/MainActivity.kt
@@ -2,15 +2,13 @@ package io.github.bukunmiola.pquiz
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController =
-            Navigation.findNavController(this, R.id.nav_host_fragment)
+
 
     }
 }
--- app/src/main/java/io/github/bukunmiola/pquiz/SignInFragment.kt
+++ app/src/main/java/io/github/bukunmiola/pquiz/SignInFragment.kt
@@ -5,30 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.github.bukunmiola.pquiz.R
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.ActionOnlyNavDirections
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.NavHostFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignInFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,22 +23,28 @@ class SignInFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignInFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        fun actionSignInFragmentToHomeFragment(): NavDirections =
+            ActionOnlyNavDirections(R.id.action_signInFragment_to_homeFragment)
+
+        fun actionSignInFragmentToSignUpFragment(): NavDirections =
+            ActionOnlyNavDirections(R.id.action_signInFragment_to_signUpFragment)
             }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.buttonLogin).setOnClickListener { goToHome() }
+        view.findViewById<TextView>(R.id.sign_up).setOnClickListener { goToSignUp() }
+
+    }
+
+    private fun goToSignUp() {
+        val action = SignInFragment.actionSignInFragmentToSignUpFragment()
+        NavHostFragment.findNavController(this).navigate(action)
+    }
+
+    private fun goToHome() {
+        val action = SignInFragment.actionSignInFragmentToHomeFragment()
+        NavHostFragment.findNavController(this).navigate(action)
     }
 }
